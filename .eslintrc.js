1781b671b28f051130a566fe20de04db3079b700
ameeribro4-sudo <ameeribro4-sudo@users.noreply.github.com>
2026-07-21 22:13:04 +0100

    chore: revert root eslintrc change (backend uses its own flat config)

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,5 @@ module.exports = {
     '@typescript-eslint/no-floating-promises': 'warn',
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
   },
 };
